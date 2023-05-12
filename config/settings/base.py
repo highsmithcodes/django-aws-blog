2f812d65a3dc3f7ba77d6b7214f9a0251bac8984
highsmithcodes <ezrahighsmith@gmail.com>
2023-05-12 15:20:43 -0600

    update base.py

--- config/settings/base.py
+++ config/settings/base.py
@@ -9,6 +9,8 @@ https://docs.djangoproject.com/en/4.2/topics/settings/
 For the full list of settings and their values, see
 https://docs.djangoproject.com/en/4.2/ref/settings/
 """
+# figure out why it's not deploying
+
 
 # Required to use heroku's database
 import dj_database_url
